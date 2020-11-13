87819bd9240dcb742cce361a62ebae0a5bc1f126
John Conde <stymiee@gmail.com>
2020-11-13 13:29:53 -0500

    Added unit tests for ACipher::__debugInfo() and ACipher::__call()

--- tests/unit/ciphers/AbstractCipherTest.php
+++ tests/unit/ciphers/AbstractCipherTest.php
@@ -24,4 +24,29 @@ class AbstractCipherTest extends TestCase
         self::assertEquals(8, strlen($getPaddedText->invokeArgs(new Aes256cbc(), ['four', 8])));
         self::assertEquals(16, strlen($getPaddedText->invokeArgs(new Aes256cbc(), ['twelvetwelve', 8])));
     }
+
+    public function testDebugInfo(): void
+    {
+        $encryptionObject = Encryption::getEncryptionObject('AES-256-CBC');
+        ob_start();
+        var_dump($encryptionObject);
+        $debugInfo = ob_get_clean();
+
+        self::assertStringContainsString(Aes256cbc::class, $debugInfo);
+        self::assertStringContainsString('["blockSize"]=>', $debugInfo);
+        self::assertStringContainsString('int(8)', $debugInfo);
+        self::assertStringContainsString('["cipher"]=>', $debugInfo);
+        self::assertStringContainsString('string(11) "AES-256-CBC"', $debugInfo);
+        self::assertStringContainsString('["ivLength"]=>', $debugInfo);
+        self::assertStringContainsString('int(16)', $debugInfo);
+    }
+
+    public function testCall(): void
+    {
+        $this->expectExceptionMessage(
+            'SM4-ECB does not require an initialization vector (IV). Do not call Encryption::generateIv().'
+        );
+        $encryption = Encryption::getEncryptionObject('sm4-ecb');
+        $encryption->generateIv();
+    }
 }
