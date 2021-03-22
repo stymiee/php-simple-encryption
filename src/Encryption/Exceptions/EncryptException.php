b32bb5331784f1c8b0ea4a3c303e1902bbf39697
John Conde <stymiee@gmail.com>
2021-03-22 13:26:21 -0400

    Updated to reflect when new exceptions were added

--- src/Encryption/Exceptions/EncryptException.php
+++ src/Encryption/Exceptions/EncryptException.php
@@ -7,6 +7,7 @@ namespace Encryption\Exceptions;
 /**
  * Class CipherNotImplementedException
  * @package Encryption\Exceptions
+ * @since 1.0.5
  */
 final class EncryptException extends EncryptionException
 {
