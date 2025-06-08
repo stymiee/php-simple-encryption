f86a7a45a3855ec77b09b2e83798542cc3a9a303
John Conde <stymiee@gmail.com>
2025-06-08 09:22:24 -0400

    Task: Simplified the logic for generating an insecure IV.

--- src/Encryption/Traits/GenerateIv.php
+++ src/Encryption/Traits/GenerateIv.php
@@ -16,7 +16,7 @@ trait GenerateIv
     /**
      * Generates an initialization vector. Defaults to using openssl_random_pseudo_bytes() but will fall back to
      * random_bytes() if false is returned. If random_bytes() fails and $allowLessSecureIv is set to TRUE an IV
-     * will be generated using random characters (not recommended for production use).
+     * will be generated using random characters.
      *
      * @param bool $allowLessSecureIv
      * @return string
@@ -41,29 +41,22 @@ trait GenerateIv
     }
 
     /**
-     * Generates a random string to be used as an initialization vector if all other methods fail. Not recommended for
-     * production use.
+     * Generates an insecure initialization vector using random characters.
+     * This is not recommended for production use.
      *
      * @param int $length
      * @return string
      * @throws GenerateIvException
      */
-    protected function generateInsecureIv(int $length): string
+    private function generateInsecureIv(int $length): string
     {
-        $permitted_chars = implode(
-            '',
-            array_merge( // @codeCoverageIgnore
-                range('A', 'z'),
-                range(0, 9),
-                str_split('~!@#$%&*()-=+{};:"<>,.?/\'')
-            )
-        );
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
         try {
-            $random = '';
+            $iv = '';
             for ($i = 0; $i < $length; $i++) {
-                $random .= $permitted_chars[random_int(0, $length - 1)];
+                $iv .= $chars[random_int(0, strlen($chars) - 1)];
             }
-            return $random;
+            return $iv;
         } catch (Exception $e) {
             throw new GenerateIvException('Unable to generate insecure initialization vector (IV)');
         }
